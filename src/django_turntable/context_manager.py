8777afe0512b48f6bc57e9ab0e1de1b43c514ffb
Ryan Thomson <git@ryanthomson.net>
2026-02-25 22:15:25 -0500

    Add basic query-counting context manager

---
+++ src/django_turntable/context_manager.py
@@ -0,0 +1,36 @@
+import logging
+from collections.abc import Callable
+from contextlib import contextmanager
+from typing import Any
+
+import django.db
+from django.db.backends.base.base import BaseDatabaseWrapper
+
+logger = logging.getLogger('django_turntable')
+
+
+@contextmanager
+def inspect_queries(connection: BaseDatabaseWrapper | None = None):
+    if connection is None:
+        connection = django.db.connection
+
+    n = 0
+
+    def wrapper(
+        execute: Callable,
+        sql: str,
+        params: Any,
+        many: bool,
+        context: dict[str, Any],
+    ):
+        nonlocal n
+        n += 1
+
+        return execute(sql, params, many, context)
+
+    try:
+        with connection.execute_wrapper(wrapper):
+            yield
+    finally:
+        if n:
+            logger.info(f'{n} queries executed')
