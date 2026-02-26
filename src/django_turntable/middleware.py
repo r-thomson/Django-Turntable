36f2d1c10622cfdf48fa9baedbd0df56b693f0f0
Ryan Thomson <git@ryanthomson.net>
2026-02-25 22:21:13 -0500

    Add TurntableMiddleware

---
+++ src/django_turntable/middleware.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.core.exceptions import MiddlewareNotUsed
+from django.http import HttpRequest, HttpResponse
+
+from .context_manager import inspect_queries
+
+
+class TurntableMiddleware:
+    def __init__(self, get_response):
+        if not settings.DEBUG:
+            raise MiddlewareNotUsed
+
+        self.get_response = get_response
+
+    def __call__(self, request: HttpRequest) -> HttpResponse:
+        with inspect_queries():
+            return self.get_response(request)
