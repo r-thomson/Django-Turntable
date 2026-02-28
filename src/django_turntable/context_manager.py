3e0aa74dcd4f75ddd789c784eb7718ab0528bd2b
Ryan Thomson <git@ryanthomson.net>
2026-02-28 00:09:37 -0500

    Add execution time tracking

--- src/django_turntable/context_manager.py
+++ src/django_turntable/context_manager.py
@@ -1,6 +1,7 @@
 import logging
 from collections.abc import Callable
 from contextlib import contextmanager
+from timeit import default_timer as timer
 from typing import Any
 
 import django.db
@@ -15,6 +16,7 @@ def inspect_queries(connection: BaseDatabaseWrapper | None = None):
         connection = django.db.connection
 
     n = 0
+    t = 0.0
 
     def wrapper(
         execute: Callable[[str, Any, bool, dict[str, Any]], Any],
@@ -23,17 +25,23 @@ def inspect_queries(connection: BaseDatabaseWrapper | None = None):
         many: bool,
         context: dict[str, Any],
     ) -> Any:
-        nonlocal n
-        n += 1
+        nonlocal n, t
 
-        return execute(sql, params, many, context)
+        n += 1
+        start = timer()
+
+        try:
+            return execute(sql, params, many, context)
+        finally:
+            end = timer()
+            t += end - start
 
     try:
         with connection.execute_wrapper(wrapper):
             yield
     finally:
         if n:
-            logger.info(f'{n} queries executed')
+            logger.info(f'{n} queries executed ({t * 1000:.3f}ms)')
 
 
 __all__ = ['inspect_queries']
