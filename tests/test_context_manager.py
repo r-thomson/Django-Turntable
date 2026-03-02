b5d463fb84f64f79029309a892b665f2e44a8224
Ryan Thomson <git@ryanthomson.net>
2026-03-01 23:01:58 -0500

    Add tests for context manager

---
+++ tests/test_context_manager.py
@@ -0,0 +1,33 @@
+from django.test import TestCase
+
+from django_turntable import inspect_queries
+
+from .models import Album, Artist
+
+
+class TestInspectQueries(TestCase):
+    def test_counts_queries_and_logs_as_context_manager(self):
+        with self.assertLogs('django_turntable', level='INFO') as logs:
+            with inspect_queries():
+                Artist.objects.create(name='Joywave')
+                Album.objects.count()
+
+        self.assertEqual(len(logs.output), 1)
+        self.assertRegex(logs.output[0], r'2 queries executed \(\d+\.\d+ms\)')
+
+    def test_counts_queries_and_logs_as_context_decorator(self):
+        @inspect_queries()
+        def execute_queries():
+            Artist.objects.create(name='Joywave')
+            Album.objects.count()
+
+        with self.assertLogs('django_turntable', level='INFO') as logs:
+            execute_queries()
+
+        self.assertEqual(len(logs.output), 1)
+        self.assertRegex(logs.output[0], r'2 queries executed \(\d+\.\d+ms\)')
+
+    def test_does_not_log_if_no_queries(self):
+        with self.assertNoLogs('django_turntable', level='INFO'):
+            with inspect_queries():
+                pass
